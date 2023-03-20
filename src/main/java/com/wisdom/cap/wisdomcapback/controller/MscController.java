f52e002150703f3a993dfe7e47eb1bff9e370ccd
CMX200228 <2485224240@qq.com>
2023-03-20 19:51:31 +0800

    图像识别模块

--- src/main/java/com/wisdom/cap/wisdomcapback/controller/MscController.java
+++ src/main/java/com/wisdom/cap/wisdomcapback/controller/MscController.java
@@ -1,11 +1,17 @@
 package com.wisdom.cap.wisdomcapback.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.iflytek.cloud.speech.*;
+import com.wisdom.cap.wisdomcapback.util.Base64Util;
+import com.wisdom.cap.wisdomcapback.util.PrintedWordUtil;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.sound.sampled.*;
 
 /**
@@ -18,7 +24,44 @@ public class MscController{
     @Value("${msc.appId}")
     private String APP_ID;
 
-    @RequestMapping(value = "/speechRecognition", method = RequestMethod.POST)
+    @Resource
+    PrintedWordUtil printedWordUtil;
+
+
+    /**
+     * 返回图片中的信息
+     * @return 图片中的信息
+     * @throws Exception 异常
+     */
+    @GetMapping(value = "/image")
+    public String getImage() throws Exception {
+        //解析返回的图片内容，获取图片的base64编码
+        String request = printedWordUtil.getRequest();
+        //将字符串转换为json对象
+        JSONObject jsonObject = JSON.parseObject(request);
+        //获取到text的值
+        String string = jsonObject
+                .getJSONObject("payload")
+                .getJSONObject("ocr_output_text")
+                .getString("text");
+
+        //将base64编码解码成UTF-8格式的字符串
+        String decode = Base64Util.decode(string);
+        JSONObject jsonObject1 = JSON.parseObject(decode);
+        JSONArray jsonArray = jsonObject1.getJSONArray("pages")
+                .getJSONObject(0)
+                .getJSONArray("lines");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObject2 = jsonArray.getJSONObject(i);
+            String content = jsonObject2.getString("content");
+            stringBuilder.append(content);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @GetMapping(value = "/speechRecognition")
     public String speechRecognition() throws Exception {
 
         // 设置音频输入流
