353342c85957928d0082c3efb5bb4bc1ac9a8703
CMX200228 <2485224240@qq.com>
2023-04-17 17:18:54 +0800

    测试成功在Linux和windows运行

--- src/test/java/com/wisdom/cap/wisdomcapback/util/VoiceRecognizeUtilTest.java
+++ src/test/java/com/wisdom/cap/wisdomcapback/util/VoiceRecognizeUtilTest.java
@@ -3,8 +3,6 @@ package com.wisdom.cap.wisdomcapback.util;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * @author 陈蒙欣
  * @date 2023/4/11 20:07
@@ -14,10 +12,12 @@ class VoiceRecognizeUtilTest {
 
     @Test
     void voiceChangeStart() {
+
         VoiceRecognizeUtil.voiceChangeStart();
     }
 
     public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
         VoiceRecognizeUtil.voiceChangeStart();
     }
 }
