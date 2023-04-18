f7225ff842bdf33816f79b6700e41148d56d17ed
上野小原 <12265762+kkndbk@user.noreply.gitee.com>
2023-04-18 22:33:48 +0800

    Tts功能完成

--- src/main/java/com/wisdom/cap/wisdomcapback/controller/TtsController.java
+++ src/main/java/com/wisdom/cap/wisdomcapback/controller/TtsController.java
@@ -1,136 +1,25 @@
 package com.wisdom.cap.wisdomcapback.controller;
 
-import com.iflytek.cloud.speech.SpeechConstant;
-import com.iflytek.cloud.speech.SpeechSynthesizer;
-import com.iflytek.cloud.speech.SpeechUtility;
-import com.iflytek.cloud.speech.SynthesizeToUriListener;
 import com.wisdom.cap.wisdomcapback.service.impl.TtsServiceImpl;
-import com.wisdom.cap.wisdomcapback.util.TtsUtil;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
-
-/**
- * @author 86138
- * @date 2023/3/21 22:00
- */
-@RestController
-@RequestMapping("/msc")
+@Controller
 public class TtsController {
-    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(TtsController.class);
-    @Value("${msc.appId}")
-    private String APP_ID;
+    @Autowired
+    private TtsServiceImpl ttsService;
 
-    @GetMapping(value = "/TTS")
-    public String changeToVoice(String data) throws IOException{
-        //官网的id
-        SpeechUtility.createUtility("appid="+APP_ID);  //这里设置在讯飞官网上设置的appid......
-        //合成监听器
-        SynthesizeToUriListener synthesizeToUriListener=TtsServiceImpl.getSynthesize();
-        //默认是生成pcm文件，所以后面有一步是转换成wav文件才能正常播放
-        String fileName=TtsServiceImpl.getFileName("tts_test.mp3");
-        TtsServiceImpl.delDone(fileName);
-
-        //1.创建SpeechSynthesizer对象
-        SpeechSynthesizer mTts=SpeechSynthesizer.createSynthesizer();
-        //2.合成参数设置,<<MSC Reference Manual>> SpeechSynthesizer类
-        //设置发音人，共有5种发音人可选，可以在讯飞官网上查看
-        mTts.setParameter(SpeechConstant.VOICE_NAME,"xiaoyan");
-        //设置语速，范围0~100
-        mTts.setParameter(SpeechConstant.SPEED,"50");
-        //设置语调，范围0~100
-        mTts.setParameter(SpeechConstant.PITCH,"50");
-        //设置音量，范围0~100
-        mTts.setParameter(SpeechConstant.VOLUME,"50");
-
-        //3.开始合成
-        //设置合成音频保存位置,下面为合成的保存位置，实际上并不需要，因此先注释掉
-        mTts.synthesizeToUri(data,fileName, synthesizeToUriListener);
-
-        //设置最长时间
-        int timeOut=30;
-        int star=0;
-
-        //检验文件是否生成
-        while(!TtsServiceImpl.checkDone(fileName)){
-            try{
-                Thread.sleep(1000);
-                star++;
-                if(star>timeOut){
-                    throw new Exception("合成超时"+timeOut+"秒！");
-                }
-            } catch (Exception e) {
-                logger.error("Exception:{}",e.getMessage());
-                return e.getMessage();
-            }
-        }
-        String mp3Path = fileName.replaceAll(".pcm",".mp3");
-        //转换文件格式
-        pcmToMP3(fileName,mp3Path);
-        //删除原来的pcm文件
-        deleteFile(fileName);
-        return mp3Path;
-    }
-
-    private void deleteFile(String filename) {
-        File file=new File(filename);
-        file.delete();
-    }
-
-    /**
-     * 将pcm转换为MP3
-     */
-    private void pcmToMP3(String src, String target) throws IOException {
-        FileInputStream fis = new FileInputStream(src);
-        FileOutputStream fos = new FileOutputStream(target);
-
-        // 计算长度
-        byte[] buf = new byte[1024 * 4];
-        int size = fis.read(buf);
-        int PCMSize = 0;
-        while (size != -1) {
-            PCMSize += size;
-            size = fis.read(buf);
-        }
-        fis.close();
-
-        // 填入参数，比特率等等。这里用的是16位单声道 8000 hz
-        TtsUtil util = new TtsUtil();
-
-        // 长度字段 = 内容的大小（PCMSize) + 头部字段的大小(不包括前面4字节的标识符RIFF以及fileLength本身的4字节)
-        util.fileLength = PCMSize + (44 - 8);
-        util.fmtHdrLength = 16;
-        util.bitsPerSample = 16;
-        util.channels = 1;
-        util.formatTag = 0x0001;
-        util.samplesPerSec = 16000;
-        util.blockAlign = (short) (util.channels * util.bitsPerSample / 8);
-        util.avgBytesPerSec = util.blockAlign * util.samplesPerSec;
-        util.dataHdrLength = PCMSize;
-
-        byte[] h = util.getHeader();
-
-        // WAV标准，头部应该是44字节
-        assert h.length == 44;
-        // write header
-        fos.write(h, 0, h.length);
-        // write data stream
-        fis = new FileInputStream(src);
-        size = fis.read(buf);
-        while (size != -1) {
-            fos.write(buf, 0, size);
-            size = fis.read(buf);
-        }
-        fis.close();
-        fos.close();
-        logger.info("Convert OK!");
+    @GetMapping("/speech")
+    public ResponseEntity<byte[]> speech(@RequestParam String text) throws IOException {
+        byte[] speechBytes = ttsService.textToSpeech(text);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename("speech.mp3").build());
+        return new ResponseEntity<>(speechBytes, headers, HttpStatus.OK);
     }
 }
--- src/main/java/com/wisdom/cap/wisdomcapback/service/impl/TtsServiceImpl.java
+++ src/main/java/com/wisdom/cap/wisdomcapback/service/impl/TtsServiceImpl.java
@@ -1,177 +1,38 @@
 package com.wisdom.cap.wisdomcapback.service.impl;
 
-import com.iflytek.cloud.speech.SpeechError;
-import com.iflytek.cloud.speech.SynthesizeToUriListener;
-import com.wisdom.cap.wisdomcapback.service.TtsService;
+import okhttp3.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+import java.io.IOException;
+import java.util.Base64;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+@Service
+public class TtsServiceImpl {
+        private static final String URL = "http://api.xfyun.cn/v1/service/v1/tts";
+        private static final String APP_ID = "f590a1b8";
+        private static final String API_KEY = "e0f3f05fce831621feaf4a7fdc51f61f";
 
-/**文字转语音实现类
- * @author 86138
- * @data
- */
-@Service("ttsService")
-public class TtsServiceImpl implements TtsService, SynthesizeToUriListener {
-    private static Map<String, Boolean> voiceFile=new HashMap<>();
-
-    /**
-     * 设置生成文件队列
-     */
-    public static void setVoice(String name,Boolean have){
-        TtsServiceImpl.voiceFile.put(name,have);
-    }
-
-    /**
-     * 查看文件是否在队列中
-     * @param name 文件名
-     * @return 是否存在
-     */
-    public static  Boolean checkDone(String name){
-        Boolean don=TtsServiceImpl.voiceFile.get(name);
-        if(don==null){
-            return false;
+        public byte[] textToSpeech(String text) throws IOException {
+            OkHttpClient client = new OkHttpClient();
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+            RequestBody body = new FormBody.Builder()
+                    .add("text", text)
+                    .build();
+            Request request = new Request.Builder()
+                    .url(URL)
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .addHeader("X-Appid", APP_ID)
+                    .addHeader("X-CurTime", String.valueOf(System.currentTimeMillis() / 1000))
+                    .addHeader("X-Param", "eyJhdWUiOiJQcmltYXJ5IiwiaXBob25lIjoiMCIsInRpbWVzdGFtcCI6IjE2MTgwMTc4MzYifQ==")
+                    .addHeader("X-CheckSum", buildCheckSum(API_KEY, String.valueOf(System.currentTimeMillis() / 1000)))
+                    .post(body)
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.body().bytes();
         }
-        return true;
-    }
 
-    /**
-     * 清除队列中的信息
-     * @param name 文件名
-     */
-    public static void delDone(String name){
-        TtsServiceImpl.voiceFile.remove(name);
-    }
-
-    /**
-     * 返回合成监听器
-     * @return 合成监听器
-     */
-    public static SynthesizeToUriListener getSynthesize(){
-        return new SynthesizeToUriListener() {
-            @Override
-            public void onBufferProgress(int i) {
-                System.out.println("当前进度: "+i+"%");
-            }
-
-            @Override
-            public void onSynthesizeCompleted(String uri, SpeechError speechError) {
-                if(speechError!=null)
-                {
-                    speechError.printStackTrace();
-                }else{
-                    System.out.println("生成文件"+uri);
-                    //将生成的文件保存到队列中
-                    TtsServiceImpl.setVoice(uri,true);
-                }
-            }
-
-            @Override
-            public void onEvent(int i, int i1, int i2, int i3, Object o, Object o1) {
-
-            }
-        };
-    }
-
-    @Override
-    public void onBufferProgress(int i) {
-        System.out.println("当前进度: "+i+"%");
-    }
-
-    @Override
-    public void onSynthesizeCompleted(String uri, SpeechError speechError) {
-        if(speechError!=null)
-        {
-            speechError.printStackTrace();
-        }else{
-            System.out.println("生成文件"+uri);
-            //将生成的文件保存到队列中
-            TtsServiceImpl.setVoice(uri,true);
+        private String buildCheckSum(String apiKey, String curTime) {
+            String md5 = DigestUtils.md5DigestAsHex((apiKey + curTime).getBytes());
+            return Base64.getEncoder().encodeToString(md5.getBytes());
         }
-    }
-
-    @Override
-    public void onEvent(int i, int i1, int i2, int i3, Object o, Object o1) {
-
-    }
-
-    /**
-     * 获取文件名
-     * @param name 文件名
-     * @return 文件路径
-     */
-    public static String getFileName(String name){
-        //获取文件名
-        StringBuilder fileName=new StringBuilder(System.getProperty("user.dir"))
-                .append(File.separator).append("src")
-                .append(File.separator).append("main")
-                //获取文件路径
-                .append(File.separator).append(name);
-
-        System.out.println(fileName);
-
-        return fileName.toString();
-    }
-
-    /**
-     * @param fileLeng  转换文件长度
-     * @param srate  采样率 - 8000,16000等
-     * @param channel 通道数量 - 单声道= 1，立体声= 2等。
-     * @param format 每个样本的位数（这里是16）
-     **/
-    public static byte[] getWAVHeader(long fileLeng, int srate, int channel, int format) {
-
-        byte[] header = new byte[44];
-        long totalDataLen = fileLeng + 36;
-        long bitrate = srate * channel * format;
-
-        header[0] = 'R';
-        header[1] = 'I';
-        header[2] = 'F';
-        header[3] = 'F';
-        header[4] = (byte) (totalDataLen & 0xff);
-        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
-        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
-        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
-        header[8] = 'W';
-        header[9] = 'A';
-        header[10] = 'V';
-        header[11] = 'E';
-        header[12] = 'f';
-        header[13] = 'm';
-        header[14] = 't';
-        header[15] = ' ';
-        header[16] = (byte) format;
-        header[17] = 0;
-        header[18] = 0;
-        header[19] = 0;
-        header[20] = 1;
-        header[21] = 0;
-        header[22] = (byte) channel;
-        header[23] = 0;
-        header[24] = (byte) (srate & 0xff);
-        header[25] = (byte) ((srate >> 8) & 0xff);
-        header[26] = (byte) ((srate >> 16) & 0xff);
-        header[27] = (byte) ((srate >> 24) & 0xff);
-        header[28] = (byte) ((bitrate / 8) & 0xff);
-        header[29] = (byte) (((bitrate / 8) >> 8) & 0xff);
-        header[30] = (byte) (((bitrate / 8) >> 16) & 0xff);
-        header[31] = (byte) (((bitrate / 8) >> 24) & 0xff);
-        header[32] = (byte) ((channel * format) / 8);
-        header[33] = 0;
-        header[34] = 16;
-        header[35] = 0;
-        header[36] = 'd';
-        header[37] = 'a';
-        header[38] = 't';
-        header[39] = 'a';
-        header[40] = (byte) (fileLeng  & 0xff);
-        header[41] = (byte) ((fileLeng >> 8) & 0xff);
-        header[42] = (byte) ((fileLeng >> 16) & 0xff);
-        header[43] = (byte) ((fileLeng >> 24) & 0xff);
-        return header;
-    }
-
 }
